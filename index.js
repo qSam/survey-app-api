203531d70d9024b222037383d55421911330a9d3
Qasim Laghari <go.jets.laghari@gmail.com>
2017-10-15 16:40:15 -0500

    Add callback fucntion

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const keys = require('./config/keys');
 const app = express();
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  })
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
 );
 
 const PORT = process.env.PORT || 5000;
