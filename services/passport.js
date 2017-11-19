e029ac2469335b0e794bf58f3c87977f9312cb9e
Qasim Laghari <go.jets.laghari@gmail.com>
2017-11-19 14:37:57 -0600

    Refactor Promises to Async Await

--- services/passport.js
+++ services/passport.js
@@ -23,21 +23,20 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       console.log('Access Token', accessToken);
       console.log('Refresh Token', refreshToken);
       console.log('Profile', profile);
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          //there is already a record
-          done(null, existingUser);
-        } else {
-          //no current record
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        //there is already a record
+        done(null, existingUser);
+      } else {
+        //no current record
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
