59e2251a9d028aa29934db9e33c298669f3950bf
Qasim Laghari <go.jets.laghari@gmail.com>
2017-12-05 16:23:17 -0600

    Refact fetchUser action with Async await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios.get('/api/currentuser').then(res =>
-      dispatch({
-        type: FETCH_USER,
-        payload: res
-      })
-    );
+export const fetchUser = () =>
+  async function(dispatch) {
+    const res = await axios.get('/api/currentuser');
+    dispatch({
+      type: FETCH_USER,
+      payload: res
+    });
   };
-};
