aff8ff7bd92a2a8dad99c2640b80bb65ce50859c
Qasim Laghari <go.jets.laghari@gmail.com>
2017-12-05 13:09:37 -0600

    Fix call to current user route

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,7 +3,7 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => {
   return function(dispatch) {
-    axios.get('/api/current_user').then(res =>
+    axios.get('/api/currentuser').then(res =>
       dispatch({
         type: FETCH_USER,
         payload: res
