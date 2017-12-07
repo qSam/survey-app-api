452a98ee2a58d2dd2f29e199696fc2eaf6d5db47
Qasim Laghari <go.jets.laghari@gmail.com>
2017-12-07 00:05:50 -0600

    Update header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,11 +5,19 @@ class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return 'Still deciding';
+        return;
       case false:
-        return 'I am logged out';
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
       default:
-        return 'I am logged in';
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
     }
   }
 
