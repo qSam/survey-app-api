5c6311865ab43b08fe60582fa1ac5808fc26701e
Qasim Laghari <go.jets.laghari@gmail.com>
2017-12-10 19:36:26 -0600

    Check auth state in Header component

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -27,7 +27,7 @@ class Header extends Component {
       <nav>
         <div className="nav-wrapper">
           <Link
-            to={this.props.users ? '/surveys' : '/'}
+            to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
           >
             Emaily
