9988849230bf1cf4a9ad7da783a8a2e4c4226bae
Qasim Laghari <go.jets.laghari@gmail.com>
2017-12-26 13:48:31 -0600

    Add new Action Creator to handle stripe token

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,3 +9,12 @@ export const fetchUser = () =>
       payload: res.data
     });
   };
+
+export const handleStripeToken = () =>
+  async function(dispatch) {
+    const res = await axios.post('/api/stripe', token);
+    dispatch({
+      type: FETCH_USER,
+      payload: res.data
+    });
+  };
