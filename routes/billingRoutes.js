4d5481533f781ac50ea3f8c56fca64a69a087167
Qasim Laghari <go.jets.laghari@gmail.com>
2017-12-29 13:42:35 -0600

    Require login for Stripe transaction

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,7 +4,11 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
     //Add post stripe logic
-    //console.log(req.body);
+    if (!req.user) {
+      return res.status(401).send({
+        error: 'You must log in for stripe transaction!'
+      });
+    }
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
