b094a7924f34f9a188e1405efb2b52c9a24f2010
Qasim Laghari <go.jets.laghari@gmail.com>
2017-12-29 13:19:10 -0600

    Add Asyn await to billing routes function

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,10 +2,10 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
+  app.post('/api/stripe', async (req, res) => {
     //Add post stripe logic
     //console.log(req.body);
-    stripe.charges.create({
+    const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
       description: '$5 for 5 credits',
