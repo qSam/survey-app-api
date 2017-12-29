113e769f056fa47f4624950c5baf97544410e182
Qasim Laghari <go.jets.laghari@gmail.com>
2017-12-29 12:55:03 -0600

    Update billing route

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,6 +4,12 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 module.exports = app => {
   app.post('/api/stripe', (req, res) => {
     //Add post stripe logic
-    console.log(req.body);
+    //console.log(req.body);
+    stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: req.body.id
+    });
   });
 };
